7b44d0fddda2973325dd1454828bfed684f52d8a
조경진 <cho881020@gmail.com>
2022-05-18 16:56:03 +0900

    뷰페이저 어댑터 제작

---
+++ app/src/main/java/kr/nepp/orderapp_20220518/adapters/MainViewPagerAdapter.kt
@@ -0,0 +1,28 @@
+package kr.nepp.orderapp_20220518.adapters
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import kr.nepp.orderapp_20220518.fragments.MyProfileFragment
+import kr.nepp.orderapp_20220518.fragments.PizzaOrderFragment
+
+class MainViewPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
+    override fun getCount() = 2
+
+    override fun getItem(position: Int): Fragment {
+        return when(position) {
+
+            0 -> PizzaOrderFragment()
+            else -> MyProfileFragment()
+
+        }
+    }
+
+    override fun getPageTitle(position: Int): CharSequence? {
+        return when(position) {
+            0 -> "피자 주문"
+            else -> "내 정보 설정"
+        }
+    }
+
+}
