4ec50d5378b2ed5fad48021597c741c677a0e3a9
조경진 <cho881020@gmail.com>
2022-05-18 17:24:18 +0900

    피자가게 리스트뷰 기본 모양 출현

--- app/src/main/java/kr/nepp/orderapp_20220518/fragments/PizzaOrderFragment.kt
+++ app/src/main/java/kr/nepp/orderapp_20220518/fragments/PizzaOrderFragment.kt
@@ -5,12 +5,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_pizza_order.*
 import kr.nepp.orderapp_20220518.R
+import kr.nepp.orderapp_20220518.adapters.PizzaStoreAdapter
 import kr.nepp.orderapp_20220518.datas.StoreData
 
 class PizzaOrderFragment : Fragment() {
 
     val mPizzaStoreList = ArrayList<StoreData>()
+
+    lateinit var mAdapter: PizzaStoreAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -30,7 +34,8 @@ class PizzaOrderFragment : Fragment() {
         mPizzaStoreList.add(  StoreData("도미노피자", "https://mblogthumb-phinf.pstatic.net/20160530_173/ppanppane_14646176567743d7hd_PNG/%B5%B5%B9%CC%B3%EB%C7%C7%C0%DA_%B7%CE%B0%ED_%282%29.png?type=w800", 4.5, "1577-3082" )  )
         mPizzaStoreList.add(  StoreData("미스터피자", "https://post-phinf.pstatic.net/MjAxODEyMDVfMzYg/MDAxNTQzOTYxOTA4NjM3.8gsStnhxz7eEc9zpt5nmSRZmI-Pzpl4NJvHYU-Dlgmcg.7Vpgk0lopJ5GoTav3CUDqmXi2-_67S5AXD0AGbbR6J4g.JPEG/IMG_1641.jpg?type=w1200", 5.0, "1577-0077" )  )
 
-
+        mAdapter = PizzaStoreAdapter(requireContext(), R.layout.store_list_item, mPizzaStoreList)
+        pizzaStoreListView.adapter = mAdapter
 
 
     }
